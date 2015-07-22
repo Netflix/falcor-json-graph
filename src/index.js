8a07518f0a0318afa0cba22a05ab4d7e68b9323b
Jafar Husain <jhusain@netflix.com>
2015-07-22 06:34:08 -0700

    Adding all functions with ability to add metadata

---
+++ src/index.js
@@ -0,0 +1,32 @@
+var pathSyntax = require("falcor-path-syntax");
+
+function sentinel(type, value, props) {
+    var copy = Object.create(null);
+    if (props != null) {
+        for(var key in props) {
+            copy[key] = props[key];
+        }
+        
+        copy["$type"] = type;
+        copy.value = value;
+        return copy;
+    }
+    else {
+        return { $type: type, value: value };
+    }    
+}
+
+module.exports = {
+    ref: function ref(path, props) {
+        return sentinel("ref", pathSyntax.fromPath(path), props);
+    },
+    atom: function atom(value, props) {
+        return sentinel("atom", value, props);        
+    },
+    error: function error(errorValue, props) {
+        return sentinel("error", errorValue, props);        
+    },
+    pathValue: function pathValue(path, value) {
+        return { path: pathSyntax.fromPath(path), value: value };
+    }
+};
