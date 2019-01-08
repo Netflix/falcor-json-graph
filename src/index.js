7329118642b27a5381e61a1132ddb24d9f661db0
Peter Marton <pmarton@netflix.com>
2019-01-08 13:35:30 -0800

    refactor(src): run prettier

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // @flow
-"use strict";
+'use strict';
 export type Primitive = string | number | boolean | null;
 export type JsonValue = Primitive | JsonMap | JsonValue[];
 export type JsonMap = { [key: string]: JsonValue | void };
@@ -15,116 +15,116 @@ export type PathSet = KeySet[];
 export type JsonGraph = { [key: string]: JsonGraphNode | void, +$type?: empty };
 export type JsonGraphNode = JsonGraph | JsonGraphLeaf;
 export type JsonGraphLeaf =
-  | JsonGraphAtom
-  | JsonGraphError
-  | JsonGraphRef
-  | Primitive;
+    | JsonGraphAtom
+    | JsonGraphError
+    | JsonGraphRef
+    | Primitive;
 
 export type JsonGraphAtomDefined<T: ?JsonValue> = JsonGraphMetadata & {
-  +$type: "atom",
-  +value: T
+    +$type: 'atom',
+    +value: T
 };
 
 export type JsonGraphAtomUndefined = JsonGraphMetadata & {
-  +$type: "atom",
-  +value?: void
+    +$type: 'atom',
+    +value?: void
 };
 
 export type JsonGraphAtom = JsonGraphMetadata & {
-  +$type: "atom",
-  +value?: ?JsonValue
+    +$type: 'atom',
+    +value?: ?JsonValue
 };
 
 export type JsonGraphError = JsonGraphMetadata & {
-  +$type: "error",
-  +value: JsonValue
+    +$type: 'error',
+    +value: JsonValue
 };
 export type JsonGraphRef = JsonGraphMetadata & {
-  +$type: "ref",
-  +value: Path
+    +$type: 'ref',
+    +value: Path
 };
 
 export type JsonGraphMetadata = {
-  +$expires?: number,
-  +$size?: number,
-  +$timestamp?: number
+    +$expires?: number,
+    +$size?: number,
+    +$timestamp?: number
 };
 
 export type JsonGraphEnvelope = {
-  jsonGraph: JsonGraph,
-  paths?: PathSet[],
-  invalidated?: PathSet[],
-  context?: JsonGraph
+    jsonGraph: JsonGraph,
+    paths?: PathSet[],
+    invalidated?: PathSet[],
+    context?: JsonGraph
 };
 
 export type PathValue = {
-  path: PathSet,
-  value: JsonGraphLeaf,
-  invalidated?: empty,
-  jsonGraph?: empty
+    path: PathSet,
+    value: JsonGraphLeaf,
+    invalidated?: empty,
+    jsonGraph?: empty
 };
 
 export type PathInvalidation = {
-  path: PathSet,
-  value?: empty,
-  invalidated: true,
-  jsonGraph?: empty
+    path: PathSet,
+    value?: empty,
+    invalidated: true,
+    jsonGraph?: empty
 };
 
 export interface IDisposable {
-  dispose(): void;
-  +isDisposed: boolean;
+    dispose(): void;
+    +isDisposed: boolean;
 }
 
 export type PartialObserver<T> = {
-  +onNext?: (value: T) => void,
-  +onError?: (error: Error) => void,
-  +onCompleted?: () => void
+    +onNext?: (value: T) => void,
+    +onError?: (error: Error) => void,
+    +onCompleted?: () => void
 };
 
 export interface IObservable<T> {
-  subscribe(
-    onNext: ?PartialObserver<T> | ((value: T) => void),
-    onError: ?(error: Error) => void,
-    onCompleted: ?() => void
-  ): IDisposable;
+    subscribe(
+        onNext: ?PartialObserver<T> | ((value: T) => void),
+        onError: ?(error: Error) => void,
+        onCompleted: ?() => void
+    ): IDisposable;
 }
 
 export interface IDataSource {
-  get(paths: PathSet[]): IObservable<JsonGraphEnvelope>;
-  set(jsonGraphEnvelope: JsonGraphEnvelope): IObservable<JsonGraphEnvelope>;
-  call(
-    functionPath: Path,
-    args?: JsonGraphNode[],
-    refSuffixes?: PathSet[],
-    thisPaths?: PathSet[]
-  ): IObservable<JsonGraphEnvelope>;
+    get(paths: PathSet[]): IObservable<JsonGraphEnvelope>;
+    set(jsonGraphEnvelope: JsonGraphEnvelope): IObservable<JsonGraphEnvelope>;
+    call(
+        functionPath: Path,
+        args?: JsonGraphNode[],
+        refSuffixes?: PathSet[],
+        thisPaths?: PathSet[]
+    ): IObservable<JsonGraphEnvelope>;
 }
 
 const {
-  ref,
-  atom,
-  undefinedAtom,
-  error,
-  pathValue,
-  pathInvalidation
-} = require("./factories");
+    ref,
+    atom,
+    undefinedAtom,
+    error,
+    pathValue,
+    pathInvalidation
+} = require('./factories');
 
 const {
-  mergeJsonGraph,
-  mergeJsonGraphEnvelope,
-  mergeJsonGraphNode
-} = require("./merge");
+    mergeJsonGraph,
+    mergeJsonGraphEnvelope,
+    mergeJsonGraphNode
+} = require('./merge');
 
 module.exports = {
-  ref,
-  atom,
-  undefinedAtom,
-  undefined: undefinedAtom,
-  error,
-  pathValue,
-  pathInvalidation,
-  mergeJsonGraph,
-  mergeJsonGraphEnvelope,
-  mergeJsonGraphNode
+    ref,
+    atom,
+    undefinedAtom,
+    undefined: undefinedAtom,
+    error,
+    pathValue,
+    pathInvalidation,
+    mergeJsonGraph,
+    mergeJsonGraphEnvelope,
+    mergeJsonGraphNode
 };
--- src/merge.js
+++ src/merge.js
@@ -1,88 +1,88 @@
 //@flow
-"use strict";
-import type { JsonGraph, JsonGraphNode, JsonGraphEnvelope } from ".";
+'use strict';
+import type { JsonGraph, JsonGraphNode, JsonGraphEnvelope } from '.';
 
 function mergeJsonGraphNode(
-  left: JsonGraphNode,
-  right: JsonGraphNode
+    left: JsonGraphNode,
+    right: JsonGraphNode
 ): JsonGraphNode {
-  if (right === null || typeof right !== "object" || right.$type) {
-    return right;
-  }
-  if (left === null || typeof left !== "object" || left.$type) {
-    return left;
-  }
-  return mergeJsonGraph(left, right);
+    if (right === null || typeof right !== 'object' || right.$type) {
+        return right;
+    }
+    if (left === null || typeof left !== 'object' || left.$type) {
+        return left;
+    }
+    return mergeJsonGraph(left, right);
 }
 
 function mergeJsonGraph(left: JsonGraph, right: JsonGraph): JsonGraph {
-  if (left === right) {
-    return right;
-  }
-  const acc: JsonGraph = Object.assign({}, left);
-  for (const key in right) {
-    if (Object.prototype.hasOwnProperty.call(right, key)) {
-      const rightValue = right[key];
-      if (typeof rightValue !== "undefined") {
-        const leftValue = acc[key];
-        if (leftValue !== rightValue) {
-          acc[key] =
-            typeof leftValue !== "undefined"
-              ? mergeJsonGraphNode(leftValue, rightValue)
-              : rightValue;
-        }
-      }
+    if (left === right) {
+        return right;
     }
-  }
-  return acc;
+    const acc: JsonGraph = Object.assign({}, left);
+    for (const key in right) {
+        if (Object.prototype.hasOwnProperty.call(right, key)) {
+            const rightValue = right[key];
+            if (typeof rightValue !== 'undefined') {
+                const leftValue = acc[key];
+                if (leftValue !== rightValue) {
+                    acc[key] =
+                        typeof leftValue !== 'undefined'
+                            ? mergeJsonGraphNode(leftValue, rightValue)
+                            : rightValue;
+                }
+            }
+        }
+    }
+    return acc;
 }
 
 function mergeJsonGraphEnvelope(
-  left: JsonGraphEnvelope,
-  right: JsonGraphEnvelope
+    left: JsonGraphEnvelope,
+    right: JsonGraphEnvelope
 ): JsonGraphEnvelope {
-  if (
-    left === right ||
-    (left.paths &&
-      left.paths.length === 0 &&
-      !left.invalidated &&
-      !left.context)
-  ) {
-    return right;
-  }
-  const result: JsonGraphEnvelope = {
-    jsonGraph: right.jsonGraph
-      ? mergeJsonGraph(left.jsonGraph, right.jsonGraph)
-      : left.jsonGraph
-  };
-
-  const leftPaths = left.paths || [];
-  const rightPaths = right.paths || [];
-
-  if (Array.isArray(left.paths) || Array.isArray(right.paths)) {
-    if (leftPaths.length && !rightPaths.length) {
-      result.paths = leftPaths;
-    } else if (!leftPaths.length && rightPaths.length) {
-      result.paths = rightPaths;
-    } else {
-      result.paths = leftPaths.concat(rightPaths);
+    if (
+        left === right ||
+        (left.paths &&
+            left.paths.length === 0 &&
+            !left.invalidated &&
+            !left.context)
+    ) {
+        return right;
     }
-  }
-  if (right.invalidated) {
-    result.invalidated = left.invalidated
-      ? left.invalidated.concat(right.invalidated)
-      : right.invalidated;
-  } else if (left.invalidated) {
-    result.invalidated = left.invalidated;
-  }
-  if (right.context) {
-    result.context = left.context
-      ? mergeJsonGraph(left.context, right.context)
-      : right.context;
-  } else if (left.context) {
-    result.context = left.context;
-  }
-  return result;
+    const result: JsonGraphEnvelope = {
+        jsonGraph: right.jsonGraph
+            ? mergeJsonGraph(left.jsonGraph, right.jsonGraph)
+            : left.jsonGraph
+    };
+
+    const leftPaths = left.paths || [];
+    const rightPaths = right.paths || [];
+
+    if (Array.isArray(left.paths) || Array.isArray(right.paths)) {
+        if (leftPaths.length && !rightPaths.length) {
+            result.paths = leftPaths;
+        } else if (!leftPaths.length && rightPaths.length) {
+            result.paths = rightPaths;
+        } else {
+            result.paths = leftPaths.concat(rightPaths);
+        }
+    }
+    if (right.invalidated) {
+        result.invalidated = left.invalidated
+            ? left.invalidated.concat(right.invalidated)
+            : right.invalidated;
+    } else if (left.invalidated) {
+        result.invalidated = left.invalidated;
+    }
+    if (right.context) {
+        result.context = left.context
+            ? mergeJsonGraph(left.context, right.context)
+            : right.context;
+    } else if (left.context) {
+        result.context = left.context;
+    }
+    return result;
 }
 
 module.exports = { mergeJsonGraph, mergeJsonGraphEnvelope, mergeJsonGraphNode };
